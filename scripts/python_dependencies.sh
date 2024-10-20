673937aaeb1230073a86660850be3bc40d6e9245
jaykkoo <adrien.gutleben@gmail.com>
2024-10-20 14:09:00 +0200

    change install file

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+sudo apt update
+
+# Install required system packages
+sudo apt install -y libpq-dev python3-dev
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/jobfinderapi/requirements.txt
