f02dbb07a1786be70293a5fd38df4a4e8c161c43
jaykkoo <adrien.gutleben@gmail.com>
2024-10-20 15:47:26 +0200

    change sh file

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["35.180.234.203"]/' /home/ubuntu/jobfinderapi/blog/settings.py
+sed -i 's/\[]/\["35.180.234.203"]/' /home/ubuntu/jobfinderapi/jobfinderapi/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
