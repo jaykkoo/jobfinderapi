b2198f717852b6640f2f86d9907ae246a8ff282b
jaykkoo <adrien.gutleben@gmail.com>
2024-10-20 15:50:58 +0200

    change sh file

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,8 +4,8 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/jobfinderapi/nginx/nginx.conf /etc/nginx/sites-available/blog
-sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
+sudo cp /home/ubuntu/jobfinderapi/nginx/nginx.conf /etc/nginx/sites-available/jobfinderapi
+sudo ln -s /etc/nginx/sites-available/blog /etc/jobfinderapi/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -3,7 +3,7 @@
 sudo apt update
 
 # Install required system packages
-sudo apt install -y libpq-dev python3-dev
+# sudo apt install -y libpq-dev python3-dev
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/jobfinderapi/requirements.txt
