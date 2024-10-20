90b6078b8f05fc2827bee42476d7c187ad336228
jaykkoo <adrien.gutleben@gmail.com>
2024-10-20 21:27:51 +0200

    change requierements

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install libpq-dev
+sudo chown -R ubuntu:ubuntu ~/jobfinderapi
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/jobfinderapi/requirements.txt
