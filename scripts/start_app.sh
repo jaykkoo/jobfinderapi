16e998819f9451499ec2fc70ac80f9310389fb9b
jaykkoo <adrien.gutleben@gmail.com>
2024-10-24 00:33:37 +0200

    rename folder

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["54.205.8.73"]/' /home/ubuntu/jobfinderapi/awscicd/settings.py
+sed -i 's/\[]/\["3.83.3.114"]/' /home/ubuntu/jobfinderapi/config/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
