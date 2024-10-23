6303a619b4b092e94b65c0a7d606ee753f8940a7
jaykkoo <adrien.gutleben@gmail.com>
2024-10-24 00:43:04 +0200

    rename folder

--- config/wsgi.py
+++ config/wsgi.py
@@ -12,6 +12,6 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobfinderapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
