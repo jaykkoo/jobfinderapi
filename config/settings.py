ed0931af65e03bebb4f16dd42ff81bcf08428b7b
jaykkoo <adrien.gutleben@gmail.com>
2024-10-25 22:29:42 +0200

    test

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/home/ubuntu/jobfinder/staticfiles"
+STATIC_ROOT = "/home/ubuntu/jobfinderapi/staticfiles"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
