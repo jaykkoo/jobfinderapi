1d40a9bb144579f64fce61e7f01e41647cd694fc
jaykkoo <agutleben@outlook.fr>
2025-03-13 17:23:52 +0100

    try collectstatic

--- config/wsgi.py
+++ config/wsgi.py
@@ -15,3 +15,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+print("SECRET_KEY:", os.getenv("SECRET_KEY"))
+print("DB_NAME:", os.getenv("DB_NAME"))
