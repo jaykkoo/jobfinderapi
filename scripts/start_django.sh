896728f97c8010e96add982b36c21c37df4a7ae3
jaykkoo <agutleben@outlook.fr>
2025-03-18 23:23:27 +0100

    toto

--- scripts/start_django.sh
+++
@@ -1,6 +0,0 @@
-echo "Starting Django Application with Apache..."
-cd /home/ubuntu/jobfinderapi/
-source /venv/bin/activate  # Activate virtualenv if used
-python manage.py migrate  # Apply any migrations
-python manage.py collectstatic --noinput  # Collect static files
-sudo systemctl restart apache2 # Restart Apache to apply changes
