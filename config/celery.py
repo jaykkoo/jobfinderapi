32294540d9814c21796814b62623a65da3edccb9
jaykkoo <agutleben@outlook.fr>
2025-03-24 10:30:42 +0100

    debu celery

--- config/celery.py
+++ config/celery.py
@@ -11,5 +11,10 @@ app = Celery('jobfinderapi')
 # Load custom settings from Django settings
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+# Set the result backend to RabbitMQ (for storing task results)
+app.conf.update(
+    result_backend='rpc://',  # Using RabbitMQ for result storage
+)
+
 # Discover tasks from all installed Django apps
 app.autodiscover_tasks()
